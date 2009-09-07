68288d7a7d6a1719c0095c605d4407e91e626187
Meteor Liu <meteor1113@gmail.com>
2009-09-07 16:09:09 +0800

    Signed-off-by: Meteor Liu <meteor1113@gmail.com>

--- elisp/init-cedet.el
+++ elisp/init-cedet.el
@@ -10,33 +10,33 @@
 
 
 ;;; cedet setting
-(require 'cedet)
-;; (semantic-load-enable-minimum-features)
-;; (semantic-load-enable-all-exuberent-ctags-support)
-(semantic-load-enable-code-helpers)
-;; (semantic-load-enable-guady-code-helpers)
-;; (semantic-load-enable-excessive-code-helpers)
-(semantic-load-enable-semantic-debugging-helpers)
-(enable-visual-studio-bookmarks)
+(when (require 'cedet nil t)
+  ;; (semantic-load-enable-minimum-features)
+  ;; (semantic-load-enable-all-exuberent-ctags-support)
+  (semantic-load-enable-code-helpers)
+  ;; (semantic-load-enable-guady-code-helpers)
+  ;; (semantic-load-enable-excessive-code-helpers)
+  (semantic-load-enable-semantic-debugging-helpers)
+  (enable-visual-studio-bookmarks)
 
-(setq semanticdb-default-save-directory (expand-file-name "~/.semanticdb"))
-;; (setq semanticdb-project-roots (list (expand-file-name "/")))
+  (setq semanticdb-default-save-directory (expand-file-name "~/.semanticdb"))
+  ;; (setq semanticdb-project-roots (list (expand-file-name "/")))
 
-;; (global-set-key [(control tab)] 'senator-complete-symbol)
-;; (global-set-key [(control tab)] 'senator-completion-menu-popup) ; C-c , SPC
-;; (global-set-key [(control tab)] 'semantic-ia-complete-symbol)
-(global-set-key [(control tab)] 'semantic-ia-complete-symbol-menu)
-(global-set-key [f11] 'eassist-switch-h-cpp)
-(global-set-key [C-f11] 'eassist-switch-h-cpp)
-(global-set-key [f12] 'semantic-ia-fast-jump)
-(global-set-key [C-f12] 'semantic-ia-fast-jump)
+  ;; (global-set-key [(control tab)] 'senator-complete-symbol)
+  ;; (global-set-key [(control tab)] 'senator-completion-menu-popup)
+  ;; (global-set-key [(control tab)] 'semantic-ia-complete-symbol)
+  (global-set-key [(control tab)] 'semantic-ia-complete-symbol-menu)
+  (global-set-key [f11] 'eassist-switch-h-cpp)
+  (global-set-key [C-f11] 'eassist-switch-h-cpp)
+  (global-set-key [f12] 'semantic-ia-fast-jump)
+  (global-set-key [C-f12] 'semantic-ia-fast-jump))
 
 
 ;;; ecb setting
-(require 'ecb)
-(setq ecb-tip-of-the-day nil)
-(setq ecb-auto-compatibility-check nil)
-(setq ecb-primary-secondary-mouse-buttons 'mouse-1--C-mouse-1)
+(when (require 'ecb nil t)
+  (setq ecb-tip-of-the-day nil)
+  (setq ecb-auto-compatibility-check nil)
+  (setq ecb-primary-secondary-mouse-buttons 'mouse-1--C-mouse-1))
 
 
 (provide 'init-cedet)
